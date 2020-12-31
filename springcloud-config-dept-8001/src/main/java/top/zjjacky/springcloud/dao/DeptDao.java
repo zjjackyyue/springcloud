175ddf5df82f73d1ef17dd255a9c631a6d3b251c
zjjacky <zjjackyyue@qq.com>
2020-12-31 16:04:29 +0800

    git环境搭建

---
+++ springcloud-config-dept-8001/src/main/java/top/zjjacky/springcloud/dao/DeptDao.java
@@ -0,0 +1,20 @@
+package top.zjjacky.springcloud.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import top.zjjacky.springcloud.api.pojo.Dept;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface DeptDao {
+
+    public boolean addDept(Dept dept);
+
+    public Dept queryDeptById(Long id);
+
+    public List<Dept> queryAll();
+}
